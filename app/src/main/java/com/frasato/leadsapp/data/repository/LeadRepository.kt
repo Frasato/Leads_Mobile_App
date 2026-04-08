ab03f7acbcbe32febfee4f200258643fd8ac4549
Frasato <gabriel.frasato@hotmail.com>
2026-04-08 16:57:29 -0300

    feat (repository): Repository for my leads list

---
+++ app/src/main/java/com/frasato/leadsapp/data/repository/LeadRepository.kt
@@ -0,0 +1,10 @@
+package com.frasato.leadsapp.data.repository
+
+import com.frasato.leadsapp.data.model.Lead
+import com.frasato.leadsapp.data.remote.RetrofitInstance
+
+class LeadRepository {
+    suspend fun getLeads(): List<Lead>{
+        return RetrofitInstance.api.getLeads();
+    }
+}
