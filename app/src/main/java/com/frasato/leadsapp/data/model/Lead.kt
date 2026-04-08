60dbeeafff5c01e938c368f65010c1257ef2a250
Frasato <gabriel.frasato@hotmail.com>
2026-04-08 16:13:57 -0300

    feat (model): Create Lead Model

---
+++ app/src/main/java/com/frasato/leadsapp/data/model/Lead.kt
@@ -0,0 +1,12 @@
+package com.frasato.leadsapp.data.model
+
+data class Lead(
+    val id: Int,
+    val name: String,
+    val phone: String,
+    val address: String,
+    val cpf: String,
+    val city: String,
+    val usedMedicine: String,
+    val addAt: String
+)
