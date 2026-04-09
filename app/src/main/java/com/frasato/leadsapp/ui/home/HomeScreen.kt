9013f48f2a29694389a19edc78cd46c50d7223c0
Frasato <gabriel.frasato@hotmail.com>
2026-04-09 09:52:10 -0300

    feat (screen): Add Surface on home to tint the background and call LeadCard

--- app/src/main/java/com/frasato/leadsapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/frasato/leadsapp/ui/home/HomeScreen.kt
@@ -1,27 +1,37 @@
 package com.frasato.leadsapp.ui.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.frasato.leadsapp.ui.components.LeadCard
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = viewModel()){
     val leads = viewModel.leads
     val loading = viewModel.isLoading
 
-    LaunchedEffect(Unit) {
-        viewModel.loadLeads()
-    }
+    LaunchedEffect(Unit) { viewModel.loadLeads() }
 
-    Column() {
-        if(loading){
-            Text("Carregando...")
-        }
-
-        leads.forEach {
-            Text(text = it.name)
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF020617)
+    ) {
+        Column(
+            modifier = Modifier.verticalScroll(rememberScrollState())
+                .padding(0.dp, 35.dp, 0.dp, 15.dp)
+        ) {
+            if(loading){ Text("Carregando...") }
+            leads.forEach { LeadCard(it) }
         }
     }
 }
