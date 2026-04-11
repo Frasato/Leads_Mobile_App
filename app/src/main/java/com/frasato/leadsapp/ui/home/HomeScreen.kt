cad84f4344c33b58283c5b4bf51267f51ea9544d
Frasato <gabriel.frasato@hotmail.com>
2026-04-11 08:35:43 -0300

    feat (home): Add bottom nav bar on home screen

--- app/src/main/java/com/frasato/leadsapp/ui/home/HomeScreen.kt
+++ app/src/main/java/com/frasato/leadsapp/ui/home/HomeScreen.kt
@@ -1,9 +1,11 @@
 package com.frasato.leadsapp.ui.home
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,32 +14,46 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.frasato.leadsapp.ui.components.BottomNavBar
 import com.frasato.leadsapp.ui.components.LeadCard
+import com.frasato.leadsapp.ui.navigation.NavRoutes
 
 @Composable
-fun HomeScreen(viewModel: HomeViewModel = viewModel()){
+fun HomeScreen(viewModel: HomeViewModel = viewModel(), navController: NavController){
     val leads = viewModel.leads
     val loading = viewModel.isLoading
 
     LaunchedEffect(Unit) { viewModel.loadLeads() }
 
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = Color(0xFF020617)
-    ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize().padding(top = 35.dp, bottom = 15.dp)
+    Scaffold(
+        bottomBar = {
+            BottomNavBar(
+                navController = navController,
+                currentRoute = NavRoutes.HOME
+            )
+        }
+    ){ paddingValues ->
+        Surface(
+            modifier = Modifier.fillMaxSize().padding(paddingValues = paddingValues),
+            color = Color(0xFF020617)
         ) {
-            if(loading){
-                item {
-                    Text(
-                        text = "Carregando...",
-                        color = Color.White,
-                        modifier = Modifier.padding(16.dp)
-                    )
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 35.dp, bottom = 15.dp)
+            ) {
+                if (loading) {
+                    item {
+                        Text(
+                            text = "Carregando...",
+                            color = Color.White,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                    }
                 }
+                items(leads) { lead -> LeadCard(lead) }
             }
-            items(leads) {lead -> LeadCard(lead)}
         }
     }
 }
