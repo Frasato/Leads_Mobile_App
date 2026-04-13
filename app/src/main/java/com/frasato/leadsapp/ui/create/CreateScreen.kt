306e4638c4260d2c3c25af4ea00adaa09da82c9f
Frasato <gabriel.frasato@hotmail.com>
2026-04-13 10:54:01 -0300

    feat: Add form on create screen

--- app/src/main/java/com/frasato/leadsapp/ui/create/CreateScreen.kt
+++ app/src/main/java/com/frasato/leadsapp/ui/create/CreateScreen.kt
@@ -1,19 +1,40 @@
 package com.frasato.leadsapp.ui.create
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.frasato.leadsapp.ui.components.BottomNavBar
 import com.frasato.leadsapp.ui.navigation.NavRoutes
 
 @Composable
-fun CreateScreen(navController: NavController){
+fun CreateScreen(navController: NavController) {
+
+    var name by remember { mutableStateOf("") }
+    var cpf by remember { mutableStateOf("") }
+    var phone by remember { mutableStateOf("") }
+    var address by remember { mutableStateOf("") }
+    var city by remember { mutableStateOf("") }
+    var useMedicine by remember { mutableStateOf("") }
+
     Scaffold(
         bottomBar = {
             BottomNavBar(
@@ -22,11 +43,92 @@ fun CreateScreen(navController: NavController){
             )
         }
     ) { paddingValues ->
+
         Surface(
-            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
             color = Color(0xFF020617)
         ) {
-            Text(text = "Form Page")
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+
+                Text(
+                    text = "Criar Lead",
+                    color = Color.White,
+                    style = MaterialTheme.typography.headlineMedium
+                )
+
+                OutlinedTextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Nome") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    OutlinedTextField(
+                        value = cpf,
+                        onValueChange = { cpf = it },
+                        label = { Text("CPF") },
+                        modifier = Modifier.weight(1f)
+                    )
+
+                    OutlinedTextField(
+                        value = phone,
+                        onValueChange = { phone = it },
+                        label = { Text("Telefone") },
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+
+                OutlinedTextField(
+                    value = address,
+                    onValueChange = { address = it },
+                    label = { Text("Endereço") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    value = city,
+                    onValueChange = { city = it },
+                    label = { Text("Cidade") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                OutlinedTextField(
+                    value = useMedicine,
+                    onValueChange = { useMedicine = it },
+                    label = { Text("Uso de medicamento?") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Button(
+                    onClick = {
+                        println("""
+                            Nome: $name
+                            CPF: $cpf
+                            Telefone: $phone
+                            Endereço: $address
+                            Cidade: $city
+                            Medicamento: $useMedicine
+                        """.trimIndent())
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(Color(0xFF60A5FA))
+
+                ) {
+                    Text("Salvar")
+                }
+            }
         }
     }
 }
