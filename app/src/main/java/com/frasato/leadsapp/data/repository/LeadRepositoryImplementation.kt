9a3e9b8e2da83fe7f8b87c3f542d53efced593db
Frasato <gabriel.frasato@hotmail.com>
2026-04-13 11:12:20 -0300

    refactor: change name to implementation and implements lead repository interface

--- app/src/main/java/com/frasato/leadsapp/data/repository/LeadRepositoryImplementation.kt
+++ app/src/main/java/com/frasato/leadsapp/data/repository/LeadRepositoryImplementation.kt
@@ -3,13 +3,14 @@ package com.frasato.leadsapp.data.repository
 import com.frasato.leadsapp.data.model.Lead
 import com.frasato.leadsapp.data.model.LeadRequest
 import com.frasato.leadsapp.data.remote.RetrofitInstance
+import com.frasato.leadsapp.domain.repository.LeadRepository
 
-class LeadRepository {
-    suspend fun getLeads(): List<Lead>{
+class LeadRepositoryImplementation: LeadRepository {
+    override suspend fun getLeads(): List<Lead>{
         return RetrofitInstance.api.getLeads();
     }
 
-    suspend fun createLead(lead: LeadRequest){
+    override suspend fun createLead(lead: LeadRequest){
         RetrofitInstance.api.createLead(lead);
     }
 }
