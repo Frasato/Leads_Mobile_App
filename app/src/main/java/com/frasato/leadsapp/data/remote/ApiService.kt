ff08c398ec84eedc860e86b247a5793150708d1a
Frasato <gabriel.frasato@hotmail.com>
2026-04-13 10:51:28 -0300

    feat: Add a POST method to create a lead

--- app/src/main/java/com/frasato/leadsapp/data/remote/ApiService.kt
+++ app/src/main/java/com/frasato/leadsapp/data/remote/ApiService.kt
@@ -1,9 +1,15 @@
 package com.frasato.leadsapp.data.remote
 
 import com.frasato.leadsapp.data.model.Lead
+import com.frasato.leadsapp.data.model.LeadRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
     @GET("api/leads")
     suspend fun getLeads(): List<Lead>
+
+    @POST("api/leads")
+    suspend fun createLead(@Body lead: LeadRequest)
 }
