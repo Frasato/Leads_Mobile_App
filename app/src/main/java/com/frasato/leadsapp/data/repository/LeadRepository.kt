221dc0fb2abfd028c395ffb7b0bb76a940f54e20
Frasato <gabriel.frasato@hotmail.com>
2026-04-13 10:52:26 -0300

    feat: Add create lead on repository

--- app/src/main/java/com/frasato/leadsapp/data/repository/LeadRepository.kt
+++ app/src/main/java/com/frasato/leadsapp/data/repository/LeadRepository.kt
@@ -1,10 +1,15 @@
 package com.frasato.leadsapp.data.repository
 
 import com.frasato.leadsapp.data.model.Lead
+import com.frasato.leadsapp.data.model.LeadRequest
 import com.frasato.leadsapp.data.remote.RetrofitInstance
 
 class LeadRepository {
     suspend fun getLeads(): List<Lead>{
         return RetrofitInstance.api.getLeads();
+    }
+
+    suspend fun createLead(lead: LeadRequest){
+        RetrofitInstance.api.createLead(lead);
     }
 }
